26190ea6916dbe919b4cbb11a69eee5368e4453a
Kris Hopper <hopperdavis@gmail.com>
2021-08-06 13:05:07 -0700

    WIP: return from lunch

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -29,7 +29,18 @@ class ReviewsController < ApplicationController
     render :show
   end
 
-  # CONTINUE CONTROLLERS
+  def update
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to products_path(@review.product)
+    else
+      @product = Product.find(params[:product_id])
+      render :edit
+    end
+  end
+
+  def destroy
+    @
 
 
   private
