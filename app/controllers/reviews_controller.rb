6fe8d29fb7c510cfaf34187cadaabbbd845911cd
Kris Hopper <hopperdavis@gmail.com>
2021-08-06 12:00:44 -0700

    add views for product pages and review controllers

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,38 @@
+class ReviewsController < ApplicationController
+
+  def new
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new
+    render :new
+  end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      flash[:notice] = "Review Added"
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    render :edit
+  end
+
+  def show
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    render :show
+  end
+
+  # CONTINUE CONTROLLERS
+
+
+  private
+    def review_params
+      params.require(:review).permit(:author, :content_body, :rating)
+  end
