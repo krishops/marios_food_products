0484b8421aabd6b31104e24cd332d12cd40266af
Kris Hopper <hopperdavis@gmail.com>
2021-08-06 11:02:22 -0700

    add code to pass review specs

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,16 @@
 class Review < ApplicationRecord
   belongs_to :product
 
+  validates :author, presence: :true 
+  validates :content_body, presence: :true
+  validates :content_body, length: { in: 50..250 }
+  validates :rating, presence: :true
+  validates_inclusion_of :rating, in: 1..5 
+
+  before_save(:titleize_review)
+
+  private
+    def titleize_review
+      self.author = self.author.titleize
+    end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -6,12 +6,13 @@ describe Review do
   it { should validate_presence_of :author }
   it { should validate_presence_of :content_body }
   it { should validate_presence_of :rating }
-  it { should validate_inclusion_of :rating, in: 1..5 }
+  it { should validate_inclusion_of(:rating).in_range(1..5) }
   it { should validate_length_of(:content_body).is_at_least(50).is_at_most(250) }
   
 
   it("titleizes the name of an author") do
-    review = Review.create({author:"jane doe", content_body: "draft", rating: 4})
+    product = Product.create({name:"Lasagna", cost: 9.99, country_of_origin: "Italy"})
+    review = Review.create({author: "jane doe", content_body: "Lorem ipsum dolor sit amet, consectetuer adipiscin", rating: 4, product_id: product.id})
     expect(review.author()).to(eq("Jane Doe"))
   end
 end
